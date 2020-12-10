28f3b28047ed2a60ef971ab645db4fa89c8f0b2e
primo9293 <42128291+primo9293@users.noreply.github.com>
2020-12-09 21:58:41 -0500

    Rutas padres e hijas y modulos -4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// Rutas Hijas
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+/* 
 import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { LoginComponent } from './auth/login/login.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { PagesComponent } from './pages/pages.component'; 
+*/
 
 const routes: Routes = [
+  
+  /* Optimización por Lazind Loading Rutas Perezosas por modulos
   {
     path: '',
     component: PagesComponent,
@@ -20,14 +30,19 @@ const routes: Routes = [
       { path: 'grafica1', component: Grafica1Component },
       { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     ]
-  },
+  }, 
+  */
   /* { path: 'dashboard', component: DashboardComponent },
   { path: 'progress', component: ProgressComponent },
-  { path: 'grafica1', component: Grafica1Component }, */
+  { path: 'grafica1', component: Grafica1Component }, 
+  */
 
+  /* 
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'register', component: RegisterComponent }, 
+  */
 
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: '**', component: NopagefoundComponent},
 ];
 
@@ -35,8 +50,11 @@ const routes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
-    // forRoot() es para rutas principales
-    RouterModule.forRoot( routes )
+    // forRoot() es para rutas principales y forChild Rutas Hijas
+    RouterModule.forRoot( routes ),
+    // Importo las rutas hijas por Modulos PagesRoutingModule - AuthRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [
     RouterModule
